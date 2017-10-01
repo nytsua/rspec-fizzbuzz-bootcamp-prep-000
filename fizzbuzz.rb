5a342815e8d3ef35649bf29e7e2cd07d220a472e
Austyn Trace <atrace@gmail.com>
2017-10-01 23:31:53 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,11 @@
 def fizzbuzz(value)
-  toReturn = nil
+  toReturn = ''
   if value % 3 == 0
     toReturn = "Fizz"
   elsif value % 5 == 0
     toReturn += "Buzz"
+  else
+    toReturn = nil
   end
   return toReturn
 end
